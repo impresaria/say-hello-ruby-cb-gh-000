87fb067722b6f052219b020636064d0e5ce92b4a
Jackie Honikman <impresaria@argsr.us>
2018-06-10 02:23:42 +0000

    automatically backed up by learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,8 +1,5 @@
-# Build your say_hello method here
-# I call on the method, say_hello, and give it the string "Jackie"
-def say_hello(name)
-  say_hello("Jackie")
-end
+# I call on the method, say_hello, and give it the string "Gabriela"
+say_hello("Gabriela")
 
 # The method prints this text to the screen:
-Hello Jackie!
+Hello Gabriela!
